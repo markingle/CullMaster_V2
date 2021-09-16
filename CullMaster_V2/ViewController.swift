4efc824238e62a0cdbdf523523d95d43997f165e
Mark Ingle <mark.ingle@omporium.com>
2021-09-16 19:55:05 -0400

    Initial Commit

---
+++ CullMaster_V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CullMaster_V2
+//
+//  Created by Mark Brady Ingle on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
